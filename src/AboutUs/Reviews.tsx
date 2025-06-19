752785a66e795387088be91928010b2408a257d3
Kevin Ritthaworn <rj.ritthaworn@icloud.com>
2025-06-19 19:31:19 -0400

    finished reviews page

--- src/AboutUs/Reviews.tsx
+++ src/AboutUs/Reviews.tsx
@@ -1,7 +1,13 @@
+import GalleryHero from "../components/GalleryHero"
+import QuoteForm from "../components/QuoteForm"
+import TestimonialSlider from "../components/TestimonialSlider"
+
 export default function Reviews() {
     return (
         <>
-            <h1>Reviews</h1>
+            <GalleryHero header="Over 50 5-Star Reviews For Our Detailing Services" button="Get A Free Quote" loc="contact" url="https://placehold.co/1920x1080"/>
+            <TestimonialSlider />
+            <QuoteForm />
         </>
     )
 }
